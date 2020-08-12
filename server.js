12a5bbd5d02d7b9a4ce06916869b78be8c736d97
Deepak290701 <deepaksharma290700@gmail.com>
2020-08-13 01:18:09 +0530

    working on frontend

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const app = express();
 const path = require("path");
 const ejs = require("ejs");
 const expressLayout = require("express-ejs-layouts");
+const { clearScreenDown } = require("readline");
 const PORT = process.env.PORT || 3000;
 
+// folder from where the frontend will be shown from
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('home');
@@ -19,3 +22,4 @@ app.set('view engine', 'ejs');
 app.listen(PORT, () => {
   console.log(`port is running on ${PORT}`)
 });
+clearScreenDown
